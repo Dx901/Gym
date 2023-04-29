6d6354210a455e6e8f8013c07045cd68396e6b88
Dyes Jr <donjnr95@gmail.com>
2023-04-29 16:15:59 +0300

    Handled the fetch Data

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,3 +1,13 @@
+export const exerciseOptions = {
+    method: 'GET',
+    // url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+    headers: {
+      'content-type': 'application/octet-stream',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
+    //   'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+
 export const fetchData = async (url, options) => {
     const res = await fetch(url, options);
     const data = await res.json();
