94483dbb3289395ad9e671ed48c6ef04415e592b
Dyes Jr <donjnr95@gmail.com>
2023-05-01 13:01:30 +0300

    Completed the pagination and search

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -6,7 +6,21 @@ import { exerciseOptions, fetchData } from "../utils/fetchData"
 import ExerciseCard from './ExerciseCard'
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
-  console.log(exercises)
+
+  const [currentPage, setCurrentPage] = useState(1)
+  const exercisesPerpage = 9
+
+  const indeOfLastExercise = currentPage * exercisesPerpage
+  const indeOfFirstExercise = indeOfLastExercise - exercisesPerpage
+  const currentExercises = exercises.slice(indeOfFirstExercise, indeOfLastExercise)
+
+  const paginate = (e, value) => {
+    setCurrentPage(value)
+
+    window.scrollTo( {top: 1800, behaviour: "smooth"})
+
+  }
+  // console.log(exercises)
   return (
     <Box id="exercises"
       sx={{mt: { lg: "110px" }}}
@@ -17,9 +31,23 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
         </Typography>
         <Stack direction="row" sx={{ gap: { lg: "110px", xs: "50px"}}}
           flexWrap="wrap" justifyContent="center">
-            {exercises.map((exercise, index) =>(
+            {currentExercises.map((exercise, index) =>(
               <ExerciseCard key={index} exercise={exercise}/>
             ))}
+          </Stack>
+
+          <Stack mt="100px" alignItems="center">
+            {exercises.length> 9 && (
+              <Pagination
+                color="standard"
+                shape="rounded"
+                defaultPage={1}
+                count={Math.ceil(exercises.length / exercisesPerpage)}
+                page={currentPage}
+                onChange={paginate}
+                // onChange={(e) => paginate(e, value)}
+                size="large"
+              />)}
           </Stack>
       </Box>
   )
